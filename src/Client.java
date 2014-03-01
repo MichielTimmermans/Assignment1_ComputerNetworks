bb8662723cac1bdb97667a838a51e5b237963c4a
MichielTimmermans <michieltimmermans@hotmail.com>
2014-03-01 13:40:50 +0100

    qsf

--- src/Client.java
+++ src/Client.java
@@ -10,8 +10,10 @@ import java.net.*;
 	        //int portNumber = Integer.parseInt(args[1]);
 	    	String hostName = "www.google.com";
 	    	int portNumber = 80;
-	    	
+	    	//System.out.println("Please provide the hostname and portnumber you wish to connect to:");
+	    	//System.out.println("Format: hostname portnumber");
 	        try (
+	        	
 	        		
 	            Socket echoSocket = new Socket(hostName, portNumber);
 	        	
@@ -23,14 +25,25 @@ import java.net.*;
 	            BufferedReader stdIn =
 	                new BufferedReader(
 	                    new InputStreamReader(System.in))
-	        ) {
-	            String userInput;
-	            while ((userInput = stdIn.readLine()) != null) {
-	                String delimiters = "[ ]";
+	        ) {	                          
+                String userInput, serverInput;
+	            while (true) {
+	            	userInput = stdIn.readLine();
+	            	System.out.println("got userinput: " + userInput);
+	            	String delimiters = "[ ]";
 	                String[] tokens = userInput.split(delimiters);
 	                if(tokens.length != 3) {
 	                	System.out.println("Wrong format, use: COMMAND URI HTTP-VERSION");
 	                }
+	                out.println(userInput);
+	                System.out.println("sent to google");  
+	                System.out.println(in.readLine());
+	                System.out.println(in.readLine());
+	                System.out.println(in.readLine());
+	                System.out.println(in.readLine());
+	                System.out.println(in.readLine());
+	                System.out.println(in.readLine());
+	                
 	                
 	            }
 	        } catch (UnknownHostException e) {
