1e0629b800fa5e7f53b7f0c51bb312a31871551d
Michman <Michman@10.92.102.122>
2014-03-11 19:57:43 +0100

    client head and get

--- src/Client.java
+++ src/Client.java
@@ -1,60 +1,134 @@
 import java.io.*;
 import java.net.*;
-	 
-	public class Client {
-	    
+
+class Client {
+	public static void main(String argv[]) throws Exception {
+		
+				
+		if(!checkValidity(argv)) {
+			return;
+		}
+		
+		String server = argv[1].split("/")[0];
 		
 		
-		public static void main(String[] args) throws IOException {
-	        //String hostName = args[0];
-	        //int portNumber = Integer.parseInt(args[1]);
-	    	String hostName = "www.google.com";
-	    	int portNumber = 80;
-	    	//System.out.println("Please provide the hostname and portnumber you wish to connect to:");
-	    	//System.out.println("Format: hostname portnumber");
-	        try (
-	        	
-	        		
-	            Socket echoSocket = new Socket(hostName, portNumber);
-	        	
-	            DataOutputStream outtoserver  =
-	                new DataOutputStream(echoSocket.getOutputStream());
-	            BufferedReader infromserver =
-	                new BufferedReader(
-	                    new InputStreamReader(echoSocket.getInputStream()));
-	            BufferedReader stdIn =
-	                new BufferedReader(
-	                    new InputStreamReader(System.in))
-	        ) {	                          
-                String userInput, serverInput;
-	            while (true) {
-	            	userInput = stdIn.readLine();
-	            	System.out.println("got userinput: " + userInput);
-	            	String delimiters = "[ ]";
-	                String[] tokens = userInput.split(delimiters);
-	                if(tokens.length != 3) {
-	                	System.out.println("Wrong format, use: COMMAND URI HTTP-VERSION");
-	                }
-	                outtoserver.writeBytes(userInput + "/n");
-	                System.out.println("sent to google");  
-	                System.out.println(infromserver.readLine());
-	                System.out.println(infromserver.readLine());
-	                System.out.println(infromserver.readLine());
-	                System.out.println(infromserver.readLine());
-	                System.out.println(infromserver.readLine());
-	                System.out.println(infromserver.readLine());
-	                
-	                
-	            }
-	        } catch (UnknownHostException e) {
-	            System.err.println("Don't know about host " + hostName);
-	            System.exit(1);
-	        } catch (IOException e) {
-	            System.err.println("Couldn't get I/O for the connection to " +
-	                hostName);
-	            System.exit(1);
-	        } 
-	    }
+		//BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
+		//Socket clientSocket = new Socket(server, 80);
+		//PrintWriter outToServer = new PrintWriter(clientSocket.getOutputStream(), true);
+		//BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+		//String serverInput;
+		
+		get(argv[1], server);
+		
+		
+		if(argv[0].equals("GET")) {
+			get(argv[1], server);
+			
+		}
+		
+		
+		
+		
+		/**
+		else if (argv[0].equals("HEAD")) {
+			head(argv[1], server);
+		}
+		
+		else if (argv[0].equals("PUT")) {
+			put(argv[1], server);
+		}
+		
+		else if (argv[0].equals("POST")) {
+			post(argv[1], server);
+		}*/
+		
+		
+			
+			/**String sentence = inFromUser.readLine();
+			outToServer.println(sentence);
+			outToServer.println("");
+			boolean gotResponse = false;
+			String webPage = "";
+			while (true) {
+				serverInput = "" + inFromServer.readLine();
+				if (!(serverInput == null || serverInput.equals("null"))) {
+					webPage = webPage + serverInput;
+					System.out.println("FROM SERVER: " + serverInput);
+					gotResponse = true;
+				} else {
+					if (gotResponse) {
+						break;
+					}
+				}
+
+			} */
+			
+
+	
+		//clientSocket.close();
 	}
 	
+	//////////////////////////////////ENKEL HTTP1.0//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	public static void get(String URL, String server) throws Exception { ///////// exception catching nog fixen
+		Socket clientSocket = new Socket(server, 80);
+		PrintWriter outToServer = new PrintWriter(clientSocket.getOutputStream(), true);
+		BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));		
+		outToServer.println("GET " + URL + " " + "HTTP/1.0");
+		outToServer.println(""); 
+		String serverInput;
+		while(( serverInput = inFromServer.readLine()) != null) System.out.println(serverInput);
+		clientSocket.close();
+	}
+	
+	///////////////////GEEN HTTP1.1 versie nodig/////////////////////////////////////////////////////////////////
+	public static void head(String URL, String server) throws Exception { /////// exception catching nog fixen
+		Socket clientSocket = new Socket(server, 80);
+		PrintWriter outToServer = new PrintWriter(clientSocket.getOutputStream(), true);
+		BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));		
+		outToServer.println("HEAD " + URL + " " + "HTTP/1.0");
+		outToServer.println(""); 
+		String serverInput;
+		while(( serverInput = inFromServer.readLine()) != null) System.out.println(serverInput);
+		clientSocket.close();
+	}
+	
+	////////////////////////ENKEL HTTP1.0////////////////////////////////////////////////////////
+	public static void put(String URL, String server) throws Exception {////////////exception catching nog fixen
+		Socket clientSocket = new Socket(server, 80);
+		PrintWriter outToServer = new PrintWriter(clientSocket.getOutputStream(), true);
+		BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));		
+		BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
+		
+		
+		outToServer.println("GET " + URL + " " + "HTTP/1.0");
+		outToServer.println(""); 
+		String serverInput;
+		while(( serverInput = inFromServer.readLine()) != null) System.out.println(serverInput);
+		clientSocket.close();
+	}
 
+	public static boolean checkValidity(String argv[]) {
+
+		
+		if (argv.length != 3) {
+			System.out.println("Wrong format, use: COMMAND URI HTTP-VERSION");
+			System.out.println("Example: 'GET www.google.com/index 1.0'");
+			return false;
+		}
+
+		
+		if (!(argv[0].equals("GET") || argv[0].equals("HEAD") || argv[0].equals("PUT") || argv[0].equals("POST"))) {
+			System.out.println("Please use a valid HTTPCommand, choose from: GET, HEAD, PUT or POST");
+			System.out.println(argv[0]);
+			return false;
+		}
+
+		if (!(argv[2].equals("HTTP/1.0") || argv[2].equals("HTTP/1.1"))) {
+			System.out.println("Please use a valid HTTPVersion. Choose from: 1.0 or 1.1");
+			System.out.println(argv[2]);
+			return false;
+		}
+		return true;
+	}
+
+}
--- src/Server.java
+++ src/Server.java
@@ -1,12 +1,30 @@
-import java.net.ServerSocket;
-import java.net.Socket;
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
 
 public class Server {
+	// handler maken, moet runnable implementeren
+	// nieuwe thread maken, en handler meegeven
 
+	
+	
 	public static void main(String argv[]) throws Exception {
-		ServerSocket server = new ServerSocket(20000);
-		
-	}
+		ServerSocket servSocket = new ServerSocket(6789);
+		while (true) {
+			Socket connectionSocket = servSocket.accept();
+			if(connectionSocket != null) {
+				Handler h = new Handler(connectionSocket);
+				Thread thread = new Thread(h);
+				thread.start();
+			}
+			//BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+			//PrintWriter outToClient = new PrintWriter(connectionSocket.getOutputStream(), true);
+			//String clientSentence = inFromClient.readLine();
+			
+			
+		}
+	} 
 	
 	
 }
