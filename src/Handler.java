9cf8450fad0693d1cd330479cf989c6ff002d349
MichielTimmermans <michieltimmermans@hotmail.com>
2014-03-12 16:53:46 +0100

    xcw

--- src/Handler.java
+++ src/Handler.java
@@ -2,65 +2,84 @@ import java.io.*;
 import java.net.*;
 
 public class Handler implements Runnable {
+	BufferedReader inFromClient;
+	PrintWriter outToClient;
+	String clientSentence;
+	Socket socket;
 
 	public Handler(Socket connectionSocket) throws Exception {
-		BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
-		PrintWriter outToClient = new PrintWriter(connectionSocket.getOutputStream(), true);
+		BufferedReader inFromClient = new BufferedReader(new InputStreamReader(
+				connectionSocket.getInputStream()));
+		PrintWriter outToClient = new PrintWriter(
+				connectionSocket.getOutputStream(), true);
 		String clientSentence = inFromClient.readLine();
+		this.socket = connectionSocket;
 
 	}
-	
-	public void decode(String sentence, PrintWriter outToClient) {
+
+	@Override
+	public void run() {
 		String delimiters = "[ ]";
-		String[] tokens = sentence.split(delimiters);
+		String[] tokens = clientSentence.split(delimiters);
 		int i = 0;
 		while (i < 3) {
 			tokens[i] = tokens[i].replaceAll("\\s+", "");
 			i++;
 		}
-		
-		if(tokens[0].equals("GET")) {
-			GET(tokens[1], outToClient);
-		}
-		else if(tokens[0].equals("HEAD")) { 
-			HEAD(tokens[1], outToClient);
-		}
-		else if(tokens[0].equals("PUT")) {
-			PUT(tokens[1], outToClient);
-		}
-		else if(tokens[0].equals("POST")) {
-			POST(tokens[1], outToClient);
-		}
-		else System.out.println("unknown error in decode method");
+
+		if (tokens[0].equals("GET")) {
+			get(tokens[1], this.outToClient);
+		} else if (tokens[0].equals("HEAD")) {
+			head(tokens[1], outToClient);
+		} else if (tokens[0].equals("PUT")) {
+			put(tokens[1], outToClient);
+		} else if (tokens[0].equals("POST")) {
+			post(tokens[1], outToClient);
+		} else
+			System.out.println("unknown error in decode method");
+		socket.close();
+		outToClient.close();
 	}
-	
-	
-	//////////////////////////////////HTTTP1.0//////////////////////////////
-	public void GET(String URI, PrintWriter outToClient) throws Exception {
+
+	// ////////////////////////////////HTTTP1.0//////////////////////////////
+	public void get(String URI, PrintWriter outToClient) throws Exception {
 		String response;
 		try {
 			response = readFile(URI);
-			
-		}
-		catch (Exception esc){
+
+		} catch (Exception esc) {
 			response = "No such file exists, please provide a valid URI.";
 		}
-		
+
 		outToClient.println(response);
 		outToClient.println("");
 	}
-	
+
+	// ////////////////////////////////HTTTP1.0//////////////////////////////
+	public void head(String URI, PrintWriter outToClient) throws Exception {
+		String response;
+		try {
+			response = readFile(URI).split("\n")[0];
+
+		} catch (Exception esc) {
+			response = "No such file exists, please provide a valid URI.";
+		}
+
+		outToClient.println(response);
+		outToClient.println("");
+	}
+
 	public String readFile(String location) throws Exception {
 		FileReader fr = new FileReader("src/filte.txt");
 		BufferedReader tr = new BufferedReader(fr);
 		String text = "";
 		String temp;
-		while((temp = tr.readLine()) != null) {
+		while ((temp = tr.readLine()) != null) {
 			text = text + temp + "\n";
 		}
 		return text;
 	}
-	
+
 	public boolean checkValidity(String sentence) {
 
 		String delimiters = "[ ]";
@@ -77,25 +96,20 @@ public class Handler implements Runnable {
 			i++;
 		}
 
-		if (!(tokens[0].equals("GET") || tokens[0].equals("HEAD") || tokens[0].equals("PUT") || tokens[0].equals("POST"))) {
-			System.out.println("Please use a valid HTTPCommand, choose from: GET, HEAD, PUT or POST");
+		if (!(tokens[0].equals("GET") || tokens[0].equals("HEAD")
+				|| tokens[0].equals("PUT") || tokens[0].equals("POST"))) {
+			System.out
+					.println("Please use a valid HTTPCommand, choose from: GET, HEAD, PUT or POST");
 			System.out.println(tokens[0]);
 			return false;
 		}
 
 		if (!(tokens[2].equals("HTTP/1.0") || tokens[2].equals("HTTP/1.1"))) {
-			System.out.println("Please use a valid HTTPVersion. Choose from: 1.0 or 1.1");
+			System.out
+					.println("Please use a valid HTTPVersion. Choose from: 1.0 or 1.1");
 			return false;
 		}
 		return true;
 	}
 
-	@Override
-	public void run() {
-		// TODO Auto-generated method stub
-		
-	}
 }
-
-	
-
